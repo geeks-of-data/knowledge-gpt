95400b966b7842a12005e1318605624169d83089
Yunus Emre Gündoğmuş <32923721+yemregundogmus@users.noreply.github.com>
2023-03-19 15:17:32 +0300

    added cloudscraper
    Normal requests'e headers eklendi. Ayrıca Cloudscraper kütüphanesi ile cloudflare v1 korumalarının bypass edilmesi sağlandı.

--- knowledgegpt/utils/utils_scrape.py
+++ knowledgegpt/utils/utils_scrape.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from fastapi import HTTPException
-
+import cloudscraper
 
 def scrape_content(url):
     """
@@ -10,9 +10,14 @@ def scrape_content(url):
     :param url: URL of the webpage
     :return: Dataframe with paragraphs
     """
+    scraper = cloudscraper.create_scraper(browser='chrome')  
 
     try:
-        response = requests.get(url)
+        response = scraper.get(url)
+        
+        if response.status_code != 200:
+            response = requests.get(url, headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"})
+        
         soup = BeautifulSoup(response.content, 'html.parser')
         scraped_content = soup.get_text()
         paragraphs = [p.strip() for p in scraped_content.split("\n\n") if p.strip()]
