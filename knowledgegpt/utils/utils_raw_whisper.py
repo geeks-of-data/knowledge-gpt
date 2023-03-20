98c3a78891a33c5ef7ca53e6a4f20c149ecc8d53
eren23 <erenak1996@gmail.com>
2023-03-20 17:44:15 +0100

    a temp fix for low quality string merging/paragraph creation

--- knowledgegpt/utils/utils_raw_whisper.py
+++ knowledgegpt/utils/utils_raw_whisper.py
@@ -4,16 +4,22 @@ import openai
 def transcribe_audio(audio_path):
 
     paragraphs = []
+    sentences_per_paragraph = 6
     
     with open(audio_path, "rb") as f:
         
         transcript = openai.Audio.transcribe("whisper-1", f)
         text = transcript["text"]
 
-        new_paragraphs = re.findall(r"(.*?\.)(?=\s|$)", text, re.DOTALL)
-        new_paragraphs = [p.strip() for p in new_paragraphs]
-        new_paragraphs = [p for p in new_paragraphs if len(p) > 0]
-        
-        paragraphs += new_paragraphs
+        # Split the text into sentences
+        sentences = re.findall(r"(.*?\.)(?=\s|$)", text, re.DOTALL)
+        sentences = [s.strip() for s in sentences]
+        sentences = [s for s in sentences if len(s) > 0]
+
+        # Group the sentences into paragraphs
+        num_sentences = len(sentences)
+        for i in range(0, num_sentences, sentences_per_paragraph):
+            paragraph = ' '.join(sentences[i:i+sentences_per_paragraph])
+            paragraphs.append(paragraph)
 
         return paragraphs
