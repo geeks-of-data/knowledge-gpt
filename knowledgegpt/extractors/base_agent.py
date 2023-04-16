26dc100ac3f370aeb8bc3b76aeb2cd1c6b31a823
eren23 <erenak1996@gmail.com>
2023-04-16 17:38:05 +0300

    self containing knowledge using agents tested

--- knowledgegpt/extractors/base_agent.py
+++ knowledgegpt/extractors/base_agent.py
@@ -1,5 +1,7 @@
 from knowledgegpt.extractors.base_extractor import BaseExtractor
 from knowledgegpt.extractors.helpers import check_embedding_extractor, check_model_lang, check_index_type
+from knowledgegpt.utils.utils_task_selection import get_task_type, get_task_template
+from knowledgegpt.utils.utils_wiki_fetcher import wiki_fetcher
 
 class BaseAgent(BaseExtractor):
     """_summary_
@@ -9,17 +11,34 @@ class BaseAgent(BaseExtractor):
     """
     
     def __init__(self, dataframe=None, embedding_extractor="hf", model_lang="en", is_turbo=False, index_type="basic",
-                 verbose=False, index_path=None, is_gpt4=False, prompt_template=None, task_type="image_generation", hf_token=None):
+                 verbose=False, index_path=None, is_gpt4=False,  hf_token=None, info_source=None, info_topic=None):
         super().__init__(dataframe=dataframe, embedding_extractor=embedding_extractor, model_lang=model_lang,
                          is_turbo=is_turbo, index_type=index_type, verbose=verbose, index_path=index_path, is_gpt4=is_gpt4,
-                         prompt_template=prompt_template)
+                        )
         
         self.hf_token = hf_token
-        self.task_type = task_type
+        self.task_type = "image_generation"
+        self.agent_first_load = True
+        self.info_source = info_source
+        self.info_topic = info_topic
         
     def agent_run(self, query, max_tokens, load_index=False):
         
+        if load_index:
+            self.agent_first_load = True
+        
+        if self.agent_first_load:
+            self.agent_first_load = False
+            task_type = get_task_type(query)
+            self.task_type = task_type
+            print("Task Type: ", task_type)
+            self.prompt_template = get_task_template(task_type)
+            if self.info_source == "wikipedia":
+                self.df  = wiki_fetcher(self.info_topic)
+                
+        
         if self.task_type == "image_generation":
+            print("Image Generation Task")
                   
             import requests
             import os
@@ -45,10 +64,17 @@ class BaseAgent(BaseExtractor):
             response = requests.post(API_URL, headers=headers, json={
                 "inputs": answer,
             })
-
+            
+            if response.status_code != 200:
+                raise Exception("Request failed: {} - {}".format(response.status_code, response.text))
+            
             image = Image.open(io.BytesIO(response.content))
             print("Image Generated for prompt:" + answer)
 
             image.save(os.path.join(working_dir, filename))
 
             return "Saved to disk:" + filename
+        
+    
+        if self.task_type == "image_captioning":
+            print("Image Captioning")
